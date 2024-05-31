d13e2c88ace0a6683a9cd1be0fa45c861b95ad9a
Carlos Henrique dos Santos <129197782+CarlosCafeJs@users.noreply.github.com>
2024-05-31 17:00:47 +0000

    Primeiro commit

---
+++ javaapispring/src/main/java/br/com/projeto/javaapispring/JavaapispringApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto.javaapispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaapispringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaapispringApplication.class, args);
+	}
+
+}
